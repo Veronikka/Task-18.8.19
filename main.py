9a100e8a15064a249e921f54554c89c4e74aa13d
Veronikka <verkhizha@ya.ru>
2023-07-12 16:31:39 +0300

    Edited elif chain

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-tickets = int(input("Сколько билетов вы хотите приобрести : "))
-
 payment = 0.0
 
+tickets = int(input("Сколько билетов вы хотите приобрести : "))
+
 for i in range(tickets):
-    age = int(input("Введите возраст "+str(i+1)+"-го посетителя :"))
-    if age >= 25:
-        payment += 1390
-    elif age >= 18:
+    age = int(input("Введите возраст "+str(i+1)+"-го посетителя : "))
+    if age < 18:
+        payment += 0
+    elif age >= 18 and age <= 25:
         payment += 990
+    elif age > 25:
+        payment += 1390
 
 if tickets > 3:
     payment *= 0.9
